2b3df4928978293e22c93d990f8259f6bfcb0331
mahonch <119926184+mahonch@users.noreply.github.com>
2023-12-11 19:56:01 +0300

    удаление из писка

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -18,8 +18,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   this.next = null;
  * }
  */
-
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function ListNode(x) {
+  this.value = x;
+  this.next = null;
 }
+
+module.exports = function removeKFromList(l, k) {
+  let current = l;
+  let prev = null;
+
+  while (current !== null) {
+    if (current.value === k) {
+      if (prev === null) {
+        l = current.next;
+      } 
+      else {
+        prev.next = current.next;
+      }
+    } 
+    else {
+      prev = current;
+    }
+    current = current.next;
+  }
+
+  return l;
+}
+
+const removeKFromList = require('./remove-from-list');
+
+const list = new ListNode(3);
+list.next = new ListNode(1);
+list.next.next = new ListNode(2);
+list.next.next.next = new ListNode(3);
+list.next.next.next.next = new ListNode(4);
+list.next.next.next.next.next = new ListNode(5);
+
+const k = 3;
+
+const updatedList = removeKFromList(list, k);
+console.log(updatedList);
